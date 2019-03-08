57480ec31b6e600c8dc1bc0fc7dc71049787f5c8
nick hansen <n-hansen@users.noreply.github.com>
2019-03-07 21:52:48 -0800

    embrace elm-format, i guess

--- src/StockMarket.elm
+++ src/StockMarket.elm
@@ -1,19 +1,27 @@
 module StockMarket exposing (..)
 
 import Dict exposing (Dict)
-import Util.Dict as Dict
 import List
 import List.Extra as List
 import Maybe
 import Result
 import Result.Extra as Result
-import Util.Result as Result
 import Tuple
+import Util.Dict as Dict
+import Util.Result as Result
+
+
 
 -- Types
 
-type alias PlayerName = String
-type alias CompanyName = String
+
+type alias PlayerName =
+    String
+
+
+type alias CompanyName =
+    String
+
 
 type alias Market =
     { playerOrder : List PlayerName
@@ -29,116 +37,155 @@ type alias Market =
     , certificateLimit : Int
     }
 
+
+
 -- Computed attributes
 
+
 playerCertificateCount : Market -> PlayerName -> Int
-playerCertificateCount {presidents, playerShares} player =
+playerCertificateCount { presidents, playerShares } player =
     Dict.get player playerShares
-        |> Maybe.map (Dict.values
-                          >> List.sum
-                          >> \shareCount -> shareCount - (Dict.values presidents
-                                                         |> List.filter (\p -> p == player)
-                                                         |> List.length
-                                                         )
-                     )
+        |> Maybe.map
+            (Dict.values
+                >> List.sum
+                >> (\shareCount ->
+                        shareCount
+                            - (Dict.values presidents
+                                |> List.filter (\p -> p == player)
+                                |> List.length
+                              )
+                   )
+            )
         |> Maybe.withDefault 0
+
 
 playerStockValue : Market -> PlayerName -> Int
-playerStockValue {playerShares, shareValues} player =
+playerStockValue { playerShares, shareValues } player =
     Dict.get player playerShares
-        |> Maybe.map ( Dict.toList
-                           >> List.map (\(company,shares) ->
-                                            Dict.get company shareValues
-                                            |> Maybe.map (\val -> val * shares)
-                                            |> Maybe.withDefault 0
-                                       )
-                           >> List.sum
-                     )
+        |> Maybe.map
+            (Dict.toList
+                >> List.map
+                    (\( company, shares ) ->
+                        Dict.get company shareValues
+                            |> Maybe.map (\val -> val * shares)
+                            |> Maybe.withDefault 0
+                    )
+                >> List.sum
+            )
         |> Maybe.withDefault 0
 
+
 playerNetWorth : Market -> PlayerName -> Int
-playerNetWorth ({playerCash} as market) player =
-    playerStockValue market player + ( Dict.get player playerCash
-                                     |> Maybe.withDefault 0
-                                     )
+playerNetWorth ({ playerCash } as market) player =
+    playerStockValue market player
+        + (Dict.get player playerCash
+            |> Maybe.withDefault 0
+          )
+
 
 companyShares : Market -> CompanyName -> Int
-companyShares {playerShares, marketShares, totalShares} company =
+companyShares { playerShares, marketShares, totalShares } company =
     totalShares
-    - (Dict.get company marketShares |> Maybe.withDefault 0)
-    - ( Dict.values playerShares
-      |> List.concatMap Dict.toList
-      |> List.filter (\(c,_) -> company == c)
-      |> List.map Tuple.second
-      |> List.sum
-      )
+        - (Dict.get company marketShares |> Maybe.withDefault 0)
+        - (Dict.values playerShares
+            |> List.concatMap Dict.toList
+            |> List.filter (\( c, _ ) -> company == c)
+            |> List.map Tuple.second
+            |> List.sum
+          )
+
+
 
 -- Mutations
 
+
 buyShareFromMarket : PlayerName -> CompanyName -> Market -> Result String Market
-buyShareFromMarket player company ({shareValues} as market) =
+buyShareFromMarket player company ({ shareValues } as market) =
     case Dict.get company shareValues of
-        Nothing -> Err <| "No share value for company " ++ company ++ "."
+        Nothing ->
+            Err <| "No share value for company " ++ company ++ "."
+
         Just shareValue ->
             market
                 |> removeMarketShare company
                 |> Result.andThen (addPlayerShare player company)
                 |> Result.andThen (payBankFromPlayer player shareValue)
 
+
 removeMarketShare : CompanyName -> Market -> Result String Market
-removeMarketShare company ({marketShares} as market) =
+removeMarketShare company ({ marketShares } as market) =
     case Dict.get company marketShares of
-        Nothing -> Err <| "No shares in the stock market for " ++ company ++ "."
-        Just 0 ->  Err <| "No shares in the stock market for " ++ company ++ "."
-        Just x -> Ok <| { market | marketShares = Dict.insert company (x-1) marketShares}
+        Nothing ->
+            Err <| "No shares in the stock market for " ++ company ++ "."
+
+        Just 0 ->
+            Err <| "No shares in the stock market for " ++ company ++ "."
+
+        Just x ->
+            Ok <| { market | marketShares = Dict.insert company (x - 1) marketShares }
+
 
 addPlayerShare : PlayerName -> CompanyName -> Market -> Result String Market
-addPlayerShare player company ({playerShares, certificateLimit} as market) =
+addPlayerShare player company ({ playerShares, certificateLimit } as market) =
     { market
-        | playerShares = playerShares
-                             |> Dict.update player ( Maybe.withDefault (Dict.singleton company 0)
-                                                         >> Dict.update company ( Maybe.withDefault 0
-                                                                                      >> \x -> Just <| x+1
-                                                                                )
-                                                         >> Just
-                                                   )
+        | playerShares =
+            playerShares
+                |> Dict.update player
+                    (Maybe.withDefault (Dict.singleton company 0)
+                        >> Dict.update company
+                            (Maybe.withDefault 0
+                                >> (\x -> Just <| x + 1)
+                            )
+                        >> Just
+                    )
     }
-    |> Ok
-    |> Result.guard (\m -> playerCertificateCount m player <= certificateLimit)
-       ("Player " ++ player ++ " already at certificate limit.")
-    |> Result.map updatePresidency
+        |> Ok
+        |> Result.guard (\m -> playerCertificateCount m player <= certificateLimit)
+            ("Player " ++ player ++ " already at certificate limit.")
+        |> Result.map updatePresidency
+
 
 updatePresidency : Market -> Market
-updatePresidency ({presidents, playerShares} as market) =
+updatePresidency ({ presidents, playerShares } as market) =
     { market
-        | presidents = presidents
-                           |> Dict.map ( \company president ->
-                                             let presidentShares =
-                                                     Dict.get2 president company playerShares
-                                                         |> Maybe.withDefault 0
-                                                 (controller, controllingShares) =
-                                                     Dict.toList playerShares
-                                                         |> List.map ( Tuple.mapSecond
-                                                                           ( Dict.get company
-                                                                                 >> Maybe.withDefault 0
-                                                                           )
-                                                                     )
-                                                         |> List.maximumBy Tuple.second
-                                                         |> Maybe.withDefault (president,presidentShares)
-                                             in if controllingShares > presidentShares
-                                                then controller
-                                                else president
-                                       )
+        | presidents =
+            presidents
+                |> Dict.map
+                    (\company president ->
+                        let
+                            presidentShares =
+                                Dict.get2 president company playerShares
+                                    |> Maybe.withDefault 0
+
+                            ( controller, controllingShares ) =
+                                Dict.toList playerShares
+                                    |> List.map
+                                        (Tuple.mapSecond
+                                            (Dict.get company
+                                                >> Maybe.withDefault 0
+                                            )
+                                        )
+                                    |> List.maximumBy Tuple.second
+                                    |> Maybe.withDefault ( president, presidentShares )
+                        in
+                        if controllingShares > presidentShares then
+                            controller
+
+                        else
+                            president
+                    )
     }
 
+
 payBankFromPlayer : PlayerName -> Int -> Market -> Result String Market
-payBankFromPlayer player amount ({playerCash, bank} as market) =
+payBankFromPlayer player amount ({ playerCash, bank } as market) =
     Dict.get player playerCash
         |> Result.fromMaybe ("No cash entry for player " ++ player ++ ".")
         |> Result.guard (\x -> x >= amount) ("Player " ++ player ++ " doesn't have enough cash.")
         |> Result.map
-           ( \currCash -> { market
-                              | bank = bank + amount
-                              , playerCash = Dict.insert player (currCash - amount) playerCash
-                          }
-           )
+            (\currCash ->
+                { market
+                    | bank = bank + amount
+                    , playerCash = Dict.insert player (currCash - amount) playerCash
+                }
+            )
--- tests/StockMarketTest.elm
+++ tests/StockMarketTest.elm
@@ -1,128 +1,170 @@
-module StockMarketTest exposing (..)
+module StockMarketTest exposing (suite)
 
 import Dict
-import Util.Dict as Dict
 import Expect
 import Result
 import StockMarket exposing (..)
 import Test exposing (..)
+import Util.Dict as Dict
+
 
 suite : Test
 suite =
     describe "StockMarket module"
         [ describe "Simple market example" <|
-              let p1 = "Alice"
-                  p2 = "Bob"
-                  p3 = "Candice"
-                  p4 = "Doug"
-                  c1 = "Grand Trunk"
-                  c2 = "Erie"
-                  c3 = "B&O"
-                  market = { playerOrder = [p1,p2,p3,p4]
-                           , companyOrder = [c1,c2,c3]
-                           , playerCash = Dict.fromList [ (p1, 100)
-                                                        , (p2, 200)
-                                                        , (p3, 500)
-                                                        , (p4, 1)]
-                           , playerShares = Dict.fromList [ (p1, Dict.fromList [ (c1, 2)
-                                                                               , (c2, 1)])
-                                                          , (p2, Dict.fromList [(c2, 4)])
-                                                          , (p4, Dict.fromList [(c3, 2)])]
-                           , presidents = Dict.fromList [ (c1, p1)
-                                                        , (c2, p2)
-                                                        , (c3, p4)]
-                           , marketShares = Dict.fromList [ (c2, 3)
-                                                          , (c3, 3)]
-                           , companyCash = Dict.fromList [ (c1, 300)
-                                                         , (c2, 150)
-                                                         , (c3, 5)]
-                           , shareValues = Dict.fromList [ (c1, 15)
-                                                         , (c2, 20)
-                                                         , (c3, 120)]
-                           , bank = 1000
-                           , totalShares = 10
-                           , certificateLimit = 3
-                           }
-              in [ describe "playerCertificateCount"
-                       [ test "test 1" <|
-                            \_ ->
-                            playerCertificateCount market p1 |> Expect.equal 2
-                       , test "test 2" <|
-                           \_ ->
-                               playerCertificateCount market p2 |> Expect.equal 3
-                       , test "test 3" <|
-                           \_ ->
-                               playerCertificateCount market p3 |> Expect.equal 0
-                       ]
-                 , describe "playerStockValue"
-                     [ test "test 1" <|
-                           \_ ->
-                           playerStockValue market p1 |> Expect.equal 50
-                     , test "test 2" <|
-                         \_ ->
-                             playerStockValue market p2 |> Expect.equal 80
-                     , test "test 3" <|
-                         \_ ->
-                             playerStockValue market p3 |> Expect.equal 0
-                     ]
-                 , describe "playerNetWorth"
-                     [ test "test 1" <|
-                           \_ ->
-                           playerNetWorth market p1 |> Expect.equal 150
-                     , test "test 2" <|
-                         \_ ->
-                             playerNetWorth market p2 |> Expect.equal 280
-                     ]
-                 , describe "companyShares"
-                     [ test "test 1" <|
-                           \_ ->
-                           companyShares market c1 |> Expect.equal 8
-                     , test "test 2" <|
-                         \_ ->
-                             companyShares market c2 |> Expect.equal 2
-                     ]
-                 , describe "buyShareFromMarket"
-                     [ test "transaction test 1" <|
-                           \_ ->
-                           buyShareFromMarket p1 c2 market
-                     |> Expect.all [ Result.map (\m -> playerStockValue m p1)
-                                         >> Expect.equal (Ok 70)
-                                   , Result.map (\m -> companyShares m c2)
-                                       >> Expect.equal (Ok 2)
-                                   , Result.map (.marketShares >> Dict.get c2)
-                                       >> Expect.equal (Ok (Just 2))
-                                   , Result.map (.playerShares >> Dict.get2 p1 c2)
-                                       >> Expect.equal (Ok (Just 2))
-                                   , Result.map (.playerCash >> Dict.get p1)
-                                       >> Expect.equal (Ok (Just 80))
-                                   , Result.map .bank
-                                       >> Expect.equal (Ok 1020)
-                                   ]
-                     , test "transaction test 2" <|
-                         \_ ->
-                             buyShareFromMarket p3 c2 market
-                     |> Expect.all [ Result.map (.marketShares >> Dict.get c2)
-                                         >> Expect.equal (Ok (Just 2))
-                                   , Result.map (.playerShares >> Dict.get2 p3 c2)
-                                       >> Expect.equal (Ok (Just 1))
-                                   ]
-                     , test "certificate limit" <|
-                         \_ ->
-                             buyShareFromMarket p2 c2 market |> Expect.err
-                     , test "cash available" <|
-                         \_ ->
-                             buyShareFromMarket p4 c2 market |> Expect.err
-                     , test "share available" <|
-                         \_ ->
-                             buyShareFromMarket p3 c1 market |> Expect.err
-                     , test "presidency transfer" <|
-                         \_ ->
-                             market
-                     |> buyShareFromMarket p3 c3
-                     |> Result.andThen (buyShareFromMarket p3 c3)
-                     |> Result.andThen (buyShareFromMarket p3 c3)
-                     |> Result.map (.presidents >> Dict.get c3)
-                     |> Expect.equal (Ok (Just p3))
-                     ]
-                 ]
+            let
+                p1 =
+                    "Alice"
+
+                p2 =
+                    "Bob"
+
+                p3 =
+                    "Candice"
+
+                p4 =
+                    "Doug"
+
+                c1 =
+                    "Grand Trunk"
+
+                c2 =
+                    "Erie"
+
+                c3 =
+                    "B&O"
+
+                market =
+                    { playerOrder = [ p1, p2, p3, p4 ]
+                    , companyOrder = [ c1, c2, c3 ]
+                    , playerCash =
+                        Dict.fromList
+                            [ ( p1, 100 )
+                            , ( p2, 200 )
+                            , ( p3, 500 )
+                            , ( p4, 1 )
+                            ]
+                    , playerShares =
+                        Dict.fromList
+                            [ ( p1
+                              , Dict.fromList
+                                    [ ( c1, 2 )
+                                    , ( c2, 1 )
+                                    ]
+                              )
+                            , ( p2, Dict.fromList [ ( c2, 4 ) ] )
+                            , ( p4, Dict.fromList [ ( c3, 2 ) ] )
+                            ]
+                    , presidents =
+                        Dict.fromList
+                            [ ( c1, p1 )
+                            , ( c2, p2 )
+                            , ( c3, p4 )
+                            ]
+                    , marketShares =
+                        Dict.fromList
+                            [ ( c2, 3 )
+                            , ( c3, 3 )
+                            ]
+                    , companyCash =
+                        Dict.fromList
+                            [ ( c1, 300 )
+                            , ( c2, 150 )
+                            , ( c3, 5 )
+                            ]
+                    , shareValues =
+                        Dict.fromList
+                            [ ( c1, 15 )
+                            , ( c2, 20 )
+                            , ( c3, 120 )
+                            ]
+                    , bank = 1000
+                    , totalShares = 10
+                    , certificateLimit = 3
+                    }
+            in
+            [ describe "playerCertificateCount"
+                [ test "test 1" <|
+                    \_ ->
+                        playerCertificateCount market p1 |> Expect.equal 2
+                , test "test 2" <|
+                    \_ ->
+                        playerCertificateCount market p2 |> Expect.equal 3
+                , test "test 3" <|
+                    \_ ->
+                        playerCertificateCount market p3 |> Expect.equal 0
+                ]
+            , describe "playerStockValue"
+                [ test "test 1" <|
+                    \_ ->
+                        playerStockValue market p1 |> Expect.equal 50
+                , test "test 2" <|
+                    \_ ->
+                        playerStockValue market p2 |> Expect.equal 80
+                , test "test 3" <|
+                    \_ ->
+                        playerStockValue market p3 |> Expect.equal 0
+                ]
+            , describe "playerNetWorth"
+                [ test "test 1" <|
+                    \_ ->
+                        playerNetWorth market p1 |> Expect.equal 150
+                , test "test 2" <|
+                    \_ ->
+                        playerNetWorth market p2 |> Expect.equal 280
+                ]
+            , describe "companyShares"
+                [ test "test 1" <|
+                    \_ ->
+                        companyShares market c1 |> Expect.equal 8
+                , test "test 2" <|
+                    \_ ->
+                        companyShares market c2 |> Expect.equal 2
+                ]
+            , describe "buyShareFromMarket"
+                [ test "transaction test 1" <|
+                    \_ ->
+                        buyShareFromMarket p1 c2 market
+                            |> Expect.all
+                                [ Result.map (\m -> playerStockValue m p1)
+                                    >> Expect.equal (Ok 70)
+                                , Result.map (\m -> companyShares m c2)
+                                    >> Expect.equal (Ok 2)
+                                , Result.map (.marketShares >> Dict.get c2)
+                                    >> Expect.equal (Ok (Just 2))
+                                , Result.map (.playerShares >> Dict.get2 p1 c2)
+                                    >> Expect.equal (Ok (Just 2))
+                                , Result.map (.playerCash >> Dict.get p1)
+                                    >> Expect.equal (Ok (Just 80))
+                                , Result.map .bank
+                                    >> Expect.equal (Ok 1020)
+                                ]
+                , test "transaction test 2" <|
+                    \_ ->
+                        buyShareFromMarket p3 c2 market
+                            |> Expect.all
+                                [ Result.map (.marketShares >> Dict.get c2)
+                                    >> Expect.equal (Ok (Just 2))
+                                , Result.map (.playerShares >> Dict.get2 p3 c2)
+                                    >> Expect.equal (Ok (Just 1))
+                                ]
+                , test "certificate limit" <|
+                    \_ ->
+                        buyShareFromMarket p2 c2 market |> Expect.err
+                , test "cash available" <|
+                    \_ ->
+                        buyShareFromMarket p4 c2 market |> Expect.err
+                , test "share available" <|
+                    \_ ->
+                        buyShareFromMarket p3 c1 market |> Expect.err
+                , test "presidency transfer" <|
+                    \_ ->
+                        market
+                            |> buyShareFromMarket p3 c3
+                            |> Result.andThen (buyShareFromMarket p3 c3)
+                            |> Result.andThen (buyShareFromMarket p3 c3)
+                            |> Result.map (.presidents >> Dict.get c3)
+                            |> Expect.equal (Ok (Just p3))
+                ]
+            ]
         ]

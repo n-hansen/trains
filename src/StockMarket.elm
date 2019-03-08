fe21d60c3dee54b47288927ce7fde5d3a6f4d94f
nick hansen <n-hansen@users.noreply.github.com>
2019-03-07 16:29:57 -0800

    nvm, oo sucks

--- src/StockMarket.elm
+++ src/StockMarket.elm
@@ -1,53 +1,60 @@
 module StockMarket exposing (..)
 
 import Dict exposing (Dict)
-import Maybe exposing (withDefault)
+import Maybe
+import Result
+import Result.Extra as Result
 
-type alias CompanyInfo =
-    { cash : Int
-    , controlledShares : Int
-    , shareValue : Int
-    , president : String
-    }
+-- Types
 
-type alias PlayerInfo =
-    { cash : Int
-    , ownedShares : Dict String Int
-    }
+type alias PlayerName = String
+type alias CompanyName = String
 
 type alias Market =
-    { companies : Dict String CompanyInfo
-    , players : Dict String PlayerInfo
-    , marketShares : Dict String Int
+    { playerOrder : List PlayerName
+    , companyOrder : List CompanyName
+    , playerCash : Dict PlayerName Int
+    , playerShares : Dict PlayerName (Dict CompanyName Int)
+    , presidents : Dict CompanyName PlayerName
+    , marketShares : Dict CompanyName Int
+    , companyCash : Dict CompanyName Int
+    , shareValues : Dict CompanyName Int
     , bank : Int
+    , totalShares : Int
     }
 
-playerCertificateCount : Market -> String -> Int
-playerCertificateCount {players, companies} player =
-    case Dict.get player players of
-        Nothing -> 0
-        Just {ownedShares} ->
-            ( Dict.values ownedShares |> List.sum ) -
-            ( Dict.values companies
-            |> List.filter (\{president} -> president == player)
-            |> List.length
-            )
+type alias Res a = Result String a
 
-playerStockValue : Market -> String -> Int
-playerStockValue {players,companies} player =
-    case Dict.get player players of
-        Nothing -> 0
-        Just {ownedShares} ->
-            Dict.toList ownedShares
-                |> List.map (\(company,count) -> count * ( Dict.get company companies
-                                                         |> Maybe.map .shareValue
-                                                         |> withDefault 0
+-- Computed attributes
+
+playerCertificateCount : Market -> PlayerName -> Int
+playerCertificateCount {presidents, playerShares} player =
+    Dict.get player playerShares
+        |> Maybe.map (Dict.values
+                          >> List.sum
+                          >> \shareCount -> shareCount - (Dict.values presidents
+                                                         |> List.filter (\p -> p == player)
+                                                         |> List.length
                                                          )
-                            )
-                |> List.sum
+                     )
+        |> Maybe.withDefault 0
 
-playerNetWorth : Market -> String -> Int
-playerNetWorth ({players} as market) player =
-    case Dict.get player players of
-        Nothing -> 0
-        Just {cash} -> cash + playerStockValue market player
+playerStockValue : Market -> PlayerName -> Int
+playerStockValue {playerShares, shareValues} player =
+    Dict.get player playerShares
+        |> Maybe.map ( Dict.toList
+                           >> List.map (\(company,shares) ->
+                                            Dict.get company shareValues
+                                            |> Maybe.map (\val -> val * shares)
+                                            |> Maybe.withDefault 0
+                                       )
+                           >> List.sum
+                     )
+        |> Maybe.withDefault 0
+
+playerNetWorth : Market -> PlayerName -> Int
+playerNetWorth ({playerCash} as market) player =
+    playerStockValue market player + ( Dict.get player playerCash
+                                     |> Maybe.withDefault 0
+                                     )
+

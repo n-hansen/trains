f3e6277f25e46fdadb1a12bcd6d5af1b2c60a665
nick hansen <n-hansen@users.noreply.github.com>
2019-03-13 23:19:56 -0700

    ParserTest typechecks

--- tests/StockMarket/ParserTest.elm
+++ tests/StockMarket/ParserTest.elm
@@ -25,17 +25,17 @@ suite =
                       checkParse
                           ["player: alice $600"]
                           { blankState
-                              | playerOrder = ["alice"]
-                              , playerCash = [("alice", 600)]
+                              | playerOrder = [P "alice"]
+                              , playerCash = [(P "alice", 600)]
                           }
               , test "playerParser 2" <|
                   \_ ->
                       checkParse
                           ["player: alice* $500"]
                           { blankState
-                                | playerOrder = ["alice"]
-                                , playerCash = [("alice", 500)]
-                                , activePlayer = Just "alice"
+                                | playerOrder = [P "alice"]
+                                , playerCash = [(P "alice", 500)]
+                                , activePlayer = Just <| P "alice"
                           }
               , test "playerParser 3" <|
                   \_ ->
@@ -44,28 +44,28 @@ suite =
                           ,"player: bob $300"
                           ]
                           { blankState
-                                | playerOrder = ["bob","alice"]
-                                , playerCash = [("bob",300)
-                                               ,("alice",200)]
+                                | playerOrder = [P "bob",P "alice"]
+                                , playerCash = [(P "bob",300)
+                                               ,(P "alice",200)]
                           }
               , test "playerParser 4" <|
                   \_ ->
                       checkParse
                           ["player: alice $200, B&O=2"]
                           { blankState
-                                | playerOrder = ["alice"]
-                                , playerCash = [("alice",200)]
-                                , playerShares = [("alice","B&O",2)]
+                                | playerOrder = [P "alice"]
+                                , playerCash = [(P "alice",200)]
+                                , playerShares = [((P "alice",C "B&O"),2)]
                           }
               , test "playerParser 5" <|
                   \_ ->
                       checkParse
                           ["player: alice $200, B&O=2, C&O=3"]
                           { blankState
-                              | playerOrder = ["alice"]
-                              , playerCash = [("alice",200)]
-                              , playerShares = [("alice","B&O",2)
-                                               ,("alice","C&O",3)
+                              | playerOrder = [P "alice"]
+                              , playerCash = [(P "alice",200)]
+                              , playerShares = [((P "alice",C "B&O"),2)
+                                               ,((P "alice",C "C&O"),3)
                                                ]
                           }
               , test "playerParser 6" <|
@@ -73,10 +73,10 @@ suite =
                       checkParse
                           ["player: alice $200, B&O*=2"]
                           { blankState
-                                | playerOrder = ["alice"]
-                                , playerCash = [("alice",200)]
-                                , playerShares = [("alice","B&O",2)]
-                                , presidents = [("B&O","alice")]
+                                | playerOrder = [P "alice"]
+                                , playerCash = [(P "alice",200)]
+                                , playerShares = [((P "alice",C "B&O"),2)]
+                                , presidents = [(C "B&O", P "alice")]
                           }
 
               , test "companyParser 1" <|
@@ -84,27 +84,27 @@ suite =
                       checkParse
                           ["company: C&O $100"]
                           { blankState
-                              | companyOrder = ["C&O"]
-                              , shareValues = [("C&O",100)]
+                              | companyOrder = [C "C&O"]
+                              , shareValues = [(C "C&O",100)]
                           }
               , test "companyParser 2" <|
                   \_ ->
                       checkParse
                           ["company: C&O $100, alice=2"]
                           { blankState
-                              | companyOrder = ["C&O"]
-                              , shareValues = [("C&O",100)]
-                              , playerShares = [("alice","C&O",2)]
+                              | companyOrder = [C "C&O"]
+                              , shareValues = [(C "C&O",100)]
+                              , playerShares = [((P "alice",C "C&O"),2)]
                           }
               , test "companyParser 3" <|
                   \_ ->
                       checkParse
                           ["company: C&O $100, alice=2, bob=3"]
                           { blankState
-                              | companyOrder = ["C&O"]
-                              , shareValues = [("C&O",100)]
-                              , playerShares = [("alice","C&O",2)
-                                               ,("bob","C&O",3)
+                              | companyOrder = [C "C&O"]
+                              , shareValues = [(C "C&O",100)]
+                              , playerShares = [((P "alice", C "C&O"),2)
+                                               ,((P "bob", C "C&O"),3)
                                                ]
                           }
               , test "companyParser 4" <|
@@ -112,22 +112,22 @@ suite =
                       checkParse
                           ["company: C&O $100, treasury=1, alice=2, bob=3"]
                           { blankState
-                              | companyOrder = ["C&O"]
-                              , shareValues = [("C&O",100)]
-                              , playerShares = [("alice","C&O",2)
-                                               ,("bob","C&O",3)
+                              | companyOrder = [C "C&O"]
+                              , shareValues = [(C "C&O",100)]
+                              , playerShares = [((P "alice", C "C&O"),2)
+                                               ,((P "bob", C "C&O"),3)
                                                ]
-                              , bankShares = [("C&O",1)]
+                              , bankShares = [(C "C&O",1)]
                           }
               , test "companyParser 5" <|
                   \_ ->
                       checkParse
                           ["company: C&O $100, alice* = 2"]
                           { blankState
-                              | companyOrder = ["C&O"]
-                              , shareValues = [("C&O",100)]
-                              , playerShares = [("alice","C&O",2)]
-                              , presidents = [("C&O","alice")]
+                              | companyOrder = [C "C&O"]
+                              , shareValues = [(C "C&O",100)]
+                              , playerShares = [((P "alice",C "C&O"),2)]
+                              , presidents = [(C "C&O", P "alice")]
                           }
               , test "certificateCountParser" <|
                   \_ ->

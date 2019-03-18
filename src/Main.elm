a86978fa41423b78b5cbc6fd6d4afca3e3551a20
nick hansen <n-hansen@users.noreply.github.com>
2019-03-17 22:29:14 -0700

    hack on Main a bit

--- src/Main.elm
+++ src/Main.elm
@@ -2,64 +2,72 @@ module Main exposing (main)
 
 import AssocList as Dict
 import Browser
+import Browser.Navigation as Browser
 import Debug
 import Html.Styled as Html exposing (Html)
+import Maybe
+import Maybe.Extra as Maybe
+import Platform.Cmd as Cmd
+import Platform.Sub as Sub
 import Result
 import StockMarket as SM exposing (Action(..),PlayerName(..),CompanyName(..))
 import StockMarket.Render as SM
 import StockMarket.Render.Configure as Cfg
 import StockMarket.Render.Types as SM
+import StockMarket.Parser as SM
+import Url exposing (Url)
 
 
 main =
-    Browser.sandbox { init = init
-                    , view = view >> Html.toUnstyled
-                    , update = update
-                    }
+    Browser.application { init = init
+                        , view = view >> Html.toUnstyled >> \b -> {title="Trains", body = [b]}
+                        , update = update
+                        , subscriptions = always Sub.none
+                        , onUrlRequest = always VoidMsg
+                        , onUrlChange = always VoidMsg
+                        }
 
 
 type Model =
     Spreadsheet { market : SM.Market
                 , projectionInput : SM.ProjectionInput
                 }
-    | Configuration Cfg.State
+    | Configuration
+      { state : Cfg.State
+      , url : Url
+      , key : Browser.Key
+      }
 
 
-init : Model
-init = Configuration Cfg.blankState
-    -- let
-    --     emptyMarket = SM.emptyMarket (SM.linearShareValueTrack [0,10,25,40,50,60,75]) 1000 5 8
-    --     market =
-    --         emptyMarket
-    --             |> SM.addPlayer (P "Alice") 200
-    --             |> SM.addPlayer (P "Bob") 300
-    --             |> SM.addCompany(C "Grand Trunk") 25
-    --             |> SM.addCompany(C "NYC") 40
-    --             |> SM.tryAction (SM.Batch
-    --                                  [ SM.BuyShareFromCompany (P "Alice") <| C "Grand Trunk"
-    --                                  , SM.BuyShareFromCompany (P "Alice") <| C "Grand Trunk"
-    --                                  , SM.BuyShareFromCompany (P "Bob" ) <| C "NYC"
-    --                                  , SM.BuyShareFromCompany (P "Bob" ) <| C "NYC"
-    --                                  ]
-    --                             )
-    --             |> Result.withDefault emptyMarket
-    --     projectionInput = Dict.empty
-    -- in
-    --     { market = market
-    --     , projectionInput = projectionInput
-    --     }
+init : () -> Url -> Browser.Key -> (Model, Cmd.Cmd msg)
+init flags url key =
+    case
+        url.query
+            |> Maybe.andThen Url.percentDecode
+            |> Maybe.andThen (SM.parseAndMaterializeMarket >> Result.toMaybe)
+    of
+        Just market ->
+            ( Spreadsheet { market = market, projectionInput = Dict.empty }
+            , Cmd.none
+            )
+
+        Nothing ->
+            ( Configuration { state = Cfg.blankState, url = url, key = key }
+            , Cmd.none
+            )
 
 
 type Message = SubmitConfiguration SM.Market
              | UpdateConfiguration Cfg.State
              | MarketAction SM.Action
              | UpdateProjection SM.ProjectionInput
+             | VoidMsg
 
 
 view model =
     case model of
         Configuration cfg ->
-            Cfg.configurationInput UpdateConfiguration SubmitConfiguration cfg
+            Cfg.configurationInput UpdateConfiguration SubmitConfiguration cfg.state
 
         Spreadsheet {market, projectionInput} ->
             SM.createRenderContext
@@ -72,28 +80,37 @@ view model =
 
 update message model =
     case model of
-        Configuration cfg ->
+        Configuration ({url} as cfg) ->
             case message of
                 UpdateConfiguration newCfg ->
-                    Configuration newCfg
+                    ( Configuration { cfg | state = newCfg } , Cmd.none )
 
                 SubmitConfiguration mkt ->
-                    Spreadsheet { market = mkt
-                                , projectionInput = Dict.empty
-                                }
+                    ( Spreadsheet { market = mkt
+                                  , projectionInput = Dict.empty
+                                  }
+                    , SM.serialize mkt
+                        |> Url.percentEncode
+                        |> (\q -> {url | query = Just q})
+                        |> Url.toString
+                        |> Browser.pushUrl cfg.key
+                    )
 
-                _ -> model
+                _ -> ( model, Cmd.none )
 
         Spreadsheet ({market, projectionInput} as ss) ->
-            Spreadsheet <|
-                case message of
-                    MarketAction axn ->
-                        market
-                            |> SM.tryAction axn
-                            |> Result.map (\m -> { ss | market = m })
-                            |> Result.withDefault ss
+            ( Spreadsheet <|
+                  case message of
+                      MarketAction axn ->
+                          market
+                              |> SM.tryAction axn
+                              |> Result.map (\m -> { ss | market = m })
+                              -- |> Result.mapError (Debug.log ">>")
+                              |> Result.withDefault ss
 
-                    UpdateProjection proj ->
-                        { ss | projectionInput = proj }
+                      UpdateProjection proj ->
+                          { ss | projectionInput = proj }
 
-                    _ -> ss
+                      _ -> ss
+            , Cmd.none
+            )
